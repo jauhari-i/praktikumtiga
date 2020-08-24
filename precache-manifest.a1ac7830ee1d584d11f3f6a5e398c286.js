68ebc39a1821359e90d5be525eb459958861dabe
Irfan Jauhari <kangrykong123@gmail.com>
2020-08-25 01:10:41 +0700

    Updates

---
+++ precache-manifest.a1ac7830ee1d584d11f3f6a5e398c286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ba8fd9b4e8b6e24dc5e7126ffadaa9",
+    "url": "/praktikumtiga/index.html"
+  },
+  {
+    "revision": "d88076e5d27b5ee000fb",
+    "url": "/praktikumtiga/static/css/main.8bad8c69.chunk.css"
+  },
+  {
+    "revision": "f74220417666cfc71676",
+    "url": "/praktikumtiga/static/js/2.0a402d4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/praktikumtiga/static/js/2.0a402d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d88076e5d27b5ee000fb",
+    "url": "/praktikumtiga/static/js/main.528dd2bb.chunk.js"
+  },
+  {
+    "revision": "faebda0ce1fb8869e447",
+    "url": "/praktikumtiga/static/js/runtime-main.94278de3.js"
+  }
+]);
